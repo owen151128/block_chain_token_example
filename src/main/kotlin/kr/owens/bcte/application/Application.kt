da0520a3677ae974655835eaf681324d864a8c89
owen151128 <owen151128@gmail.com>
2021-04-19 14:27:26 +0900

    Add example to main
    1. create blockchain & genesis block
    2. add 400 block
    3. add tampering  example(comment)
    4. verify token chain

--- src/main/kotlin/kr/owens/bcte/application/Application.kt
+++ src/main/kotlin/kr/owens/bcte/application/Application.kt
@@ -1,13 +1,44 @@
 package kr.owens.bcte.application
 
+import kr.owens.bcte.module.OwenTokenModule
+
 /**
- * @author owen151128@everspin.co.kr
+ * @author owen151128@gmail.com
  *
  * Created by owen151128 on 2021/04/16 10:14
  *
  * Providing features related to Application class
  */
-fun main(args: Array<String>) {
-    println("Hello World!")
-    println(args.contentToString())
+fun main() {
+    val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    val getRandomString = {
+        (1..10).map { kotlin.random.Random.nextInt(0, charPool.size) }.map(charPool::get)
+            .joinToString("")
+    }
+
+    val module = OwenTokenModule()
+    module.createGenesisBlock()
+    for (i in 1..400) {
+        module.addNormalBlock(getRandomString())
+    }
+
+//    val tamperIndex = 231
+//    val tamperHashResult = module.createHash(
+//        "owen",
+//        module.tokenChain[tamperIndex - 1].currentHash,
+//        module.tokenChain[tamperIndex - 1].difficulty
+//    )
+//    module.tamperToken(
+//        tamperIndex,
+//        OwenToken(
+//            "owen",
+//            module.tokenChain[tamperIndex - 1].currentHash,
+//            module.tokenChain[tamperIndex - 1].difficulty,
+//            tamperHashResult.first,
+//            tamperHashResult.second
+//        )
+//    )
+
+    module.printTokenChain()
+    println("Token verify result : ${module.verifyTokenChain()}")
 }
